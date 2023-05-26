57319082743227ef1f3d7eca8117fa332378e018
Yinshangyi <jl.dejaeger@gmail.com>
2023-05-26 22:15:02 +0200

    Pass the key as parameter for the deploy script

--- deploy_function.sh
+++ deploy_function.sh
@@ -6,5 +6,5 @@ reco-product \
 --trigger-topic Shopping_Cart_Abandonment \
 --runtime python39 \
 --memory=2048MB \
---set-env-vars SENDGRID_API_KEY=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
-rm src/requirements.txt
+--set-env-vars SENDGRID_API_KEY=$1
+rm requirements.txt
