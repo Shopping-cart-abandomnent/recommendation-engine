e912de64cebcf34ce060f5f326f3059acda10e52
Yinshangyi <jl.dejaeger@gmail.com>
2023-05-26 18:33:12 +0200

    Remove API key

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
+import base64
+import json
 import os
 
 from email_sender import send_email
-
 # Définition de la variable d'environnement GOOGLE_APPLICATION_CREDENTIALS
-#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "auth.json"
-os.environ["SENDGRID_API_KEY"] = "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"
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "auth.json"
+from src.recommendation import predict_recommendations
 
 # client = bigquery.Client()
 from google.cloud import bigquery
@@ -23,34 +24,50 @@ def get_articles_data():
 
 
 def get_articles_in_cart(products_ids):
-    query = """
+    query = f"""
         SELECT *
         FROM `valued-decker-380221.donnees_hm.articles`
-        WHERE article_id IN (448831026, 510264001, 566941026)
+        WHERE article_id IN ({products_ids[0]}, {products_ids[1]}, {products_ids[2]})
     """
     articles_in_cart = client.query(query).to_dataframe()
     return articles_in_cart
 
 
-def receive_msg(msg):
-    # msg_dict = json.loads(msg)
-    # customer_id = msg_dict["user_id"]
-    # articles_ids = msg_dict["articles_id"]
-    #
-    # products_in_carts = get_articles_in_cart(articles_ids)
-    # print("Info about products in cart acquired")
-    # all_articles = get_articles_data()
-    # print("Info about all products acquired")
-    # predicted_reco = predict_recommendations(all_articles, products_in_carts)
-    # print("Predicted products:")
-    # print(predicted_reco)
+def retrieve_user_info(user_id):
+    return {"firstname": "Jean", "lastname": "De Jaeger"}
 
-    # Send an template
-    user = {"firstname": "Jean", "lastname": "De Jaeger"}
-    products = [
+
+def retrieve_reco_products_info(reco_df):
+    return [
         {"name": "Chaussette Superman", "url": "http://this_is_product1_url.com"},
         {"name": "T-Shirt", "url": "http://this_is_product2_url.com"}
     ]
+
+
+def receive_msg(event, context):
+    """
+    The input message has the following format:
+    message = {
+        "user_id": user_id,
+        "articles_id": []
+    }
+    """
+    received_msg_json = base64.b64decode(event['data']).decode('utf-8')
+    message = json.loads(received_msg_json)
+    user_id = message["user_id"]
+    articles_ids = message["articles_id"]
+
+    products_in_carts = get_articles_in_cart(articles_ids)
+    print("Info about products in cart acquired")
+    all_articles = get_articles_data()
+    print("Info about all products acquired")
+    predicted_reco = predict_recommendations(all_articles, products_in_carts)
+    print("Predicted products:")
+    print(predicted_reco)
+
+    # Send an template
+    user = retrieve_user_info(user_id)
+    products = retrieve_reco_products_info(predicted_reco)
     send_email(user, products)
 
     # Write the recommendation in a new table
