a1954787fd5796437a350dcb4dc32782d4c8c6cf
Pierre-Ange Billa <ftnpiapia@gmail.com>
2023-05-30 23:18:27 +0200

    Updating utils with correct path

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,9 @@ def get_image_path(article_id):
     bucket_name = 'bucket_hm'
     base_folder = 'articles_image'
     article_id_str = str(article_id)
-    folder_path = article_id_str[:3]
-    image_filename = article_id_str + '.jpg'
+    article_id_padded = article_id_str.zfill(10)
+    folder_path = article_id_padded[:3]
+    image_filename = article_id_padded + '.jpg'
     image_path = f'{base_folder}/{folder_path}/{image_filename}'
     image_public_url = f"https://storage.googleapis.com/{bucket_name}/{image_path}"
     return image_public_url
